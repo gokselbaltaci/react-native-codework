45e324e567d0908342189b34e8511898f92f1c0a
Göksel Baltacı <gokselbaltaci0@gmail.com>
2022-06-13 11:30:00 +0300

    .

---
+++ src/hooks/useFetch/useFetch.js
@@ -0,0 +1,28 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const useFetch = url => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const {data: responseData} = await axios.get(url);
+      setData(responseData);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
+  };
+
+  return {error, loading, data};
+};
+
+export default useFetch;
